6b2affcd79184deb906c3964d6bde813fd727b16
Eugene <jenksbu@gmail.com>
2022-08-01 19:52:30 +0300

    done hw6

--- hw6_y_l_31_Everyone_is_equal_as_in_the_selection.py
+++ hw6_y_l_31_Everyone_is_equal_as_in_the_selection.py
@@ -28,3 +28,29 @@
     Вывод:
         different
 """
+def same_by(characteristic, objects):
+    a = []
+    for i in objects:
+        if characteristic(i) == 0:
+            a.append(True)
+        else:
+            a.append(False)
+    y = a[0]
+    b = list(filter(lambda x: x == y, a))
+    if len(a) == len(b):
+        return True
+    else:
+        return False
+
+
+values = [0, 2, 10, 6]
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')
+
+values = [1, 2, 3, 4]
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')
--- hw6_y_l_32_print_operation_table.py
+++ hw6_y_l_32_print_operation_table.py
@@ -6,7 +6,7 @@
 Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
 Нумерация строк и столбцов идёт с единицы (подумайте, почему не с нуля).
 
-Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, уоперации умножения.
+Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
     Пример 1
         Ввод: print_operation_table(lambda x, y: x * y) 1
         Вывод: см картинку
@@ -15,3 +15,25 @@
         Ввод: print_operation_table(lambda x, y: x * y, 5) 1
         Вывод: см картинку
 """
+
+def print_operation_table(operation, number_rows, number_columns):
+    if operation == '*':
+        for i in range(1, number_rows + 1):
+            for j in range(i, i * number_columns + 1, i):
+                print(j, end='\t')
+            print()
+    if operation == '+':
+        for i in range(1, number_rows + 1):
+            for j in range(i, i + number_columns + 1, i):
+                print(j, end='\t')
+            print()
+    if operation == '**':
+        for i in range(1, number_rows + 1):
+            for j in range(i, i ** number_columns + 1, i):
+                print(j, end='\t')
+            print()
+
+operation = str(input('Enter operation: '))
+number_rows = int(input('Enter count rows: '))
+number_columns = int(input('Enter count columns: '))
+print_operation_table(operation, number_rows, number_columns)
