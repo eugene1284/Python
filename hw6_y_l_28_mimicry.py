4e7b790126813e9c9ef6b91786a3a4fd09133b2f
Eugene <jenksbu@gmail.com>
2022-08-01 09:57:24 +0300

    hw6 1 done

--- hw6_y_l_28_mimicry.py
+++ hw6_y_l_28_mimicry.py
@@ -13,7 +13,8 @@
 
 Единственный способ вашего взаимодействия с этим кодом - посредством задания функции transformation.
 Однако вы поняли, что для вашей текущей задачи вам не нужно никак преобразовывать список значений, а нужно получить его как есть.
-Напишите такое лямбда-выражение transformation, чтобы transformed_values получился копией values. Переменная transformation должна быть глобальной, чтобы проверяющая система смогла его найти. Кроме transformation вам ничего писать не нужно. Печатать на экран - тоже.
+Напишите такое лямбда-выражение transformation, чтобы transformed_values получился копией values.
+Переменная transformation должна быть глобальной, чтобы проверяющая система смогла его найти. Кроме transformation вам ничего писать не нужно. Печатать на экран - тоже.
 
 
     Пример
@@ -28,3 +29,22 @@
         Вывод
         ok
 """
+def transformation(values):
+    trans_values = values
+    return trans_values
+
+
+values = [1, 23, 42, "asdfg"]
+transformed_values = list(map(transformation, values))
+if values == transformed_values:
+    print('ok')
+else:
+    print('fail')
+
+# второй вариант с лямбда
+values = [1, 23, 42, "asdfg"]
+transformed_values = list(map(lambda x: x, values))
+if values == transformed_values:
+    print('ok')
+else:
+    print('fail')
